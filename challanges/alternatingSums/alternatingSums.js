42ac4478a4ff943f988e3634e4748dd2f4ec9490
Axel <axdahlin@gmail.com>
2018-09-07 22:07:56 +0200

    add alternatingSums

---
+++ challanges/alternatingSums/alternatingSums.js
@@ -0,0 +1,11 @@
+function alternatingSums(a) {
+  let [sum1, sum2] = [0, 0];
+  for (let i = 0; i < a.length; i++) {
+    if (i % 2 === 0 ) {
+      sum1 += a[i];
+    } else {
+      sum2 += a[i];
+    }
+  }
+  return [sum1, sum2];
+}
