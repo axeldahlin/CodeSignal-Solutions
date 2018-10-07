07f5766ebfadb60d0069d762b5cc0f86fdc5c9a1
Axel <axdahlin@gmail.com>
2018-10-07 16:40:55 +0200

    add chessBoardCellColor

---
+++ challanges/chessBoardCellColor/chessBoardCellColor.js
@@ -0,0 +1,43 @@
+function chessBoardCellColor(cell1, cell2) {
+
+ 
+
+  return remainderOfPosition(cell1) === remainderOfPosition(cell2);
+
+}
+
+
+function remainderOfPosition(cell) {
+
+  let first = cell[0].toLowerCase().charCodeAt(0) - 96;
+  
+  let second = Number(cell[1]);
+
+ 
+
+  const remainder = (first + second) % 2;
+
+ 
+
+
+  console.log(cell, {first}, {second}, remainder);
+
+  return remainder;
+
+
+}
+
+
+
+
+
+
+
+const cell1 = "A1";
+const cell2 = "H3";
+
+
+console.log(chessBoardCellColor(cell1, cell2));
+
+
+
